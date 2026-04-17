2a31f3f272c537febb5f40b259fa56c14fbc9801
ꜱᴘᴧʀꜱʜ ʙᴧɴɪʏᴧ <89186991+TeamBaniya@users.noreply.github.com>
2026-04-17 13:43:56 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,20 @@
 from pyrogram import Client
-from config import BOT_TOKEN
+from config import BOT_TOKEN, API_ID, API_HASH
 from solo.handlers import register_handlers
+
+# Debug (remove later)
+print("API_ID:", API_ID)
+print("API_HASH:", API_HASH)
 
 app = Client(
     "cricket-bot",
+    api_id=int(API_ID),
+    api_hash=API_HASH,
     bot_token=BOT_TOKEN
 )
 
 register_handlers(app)
+
+print("🚀 Bot Started Successfully!")
 
 app.run()
