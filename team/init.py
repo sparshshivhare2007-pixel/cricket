637ad29b481367d1dffdc52b70ab8a03340bdbd8
ꜱᴘᴧʀꜱʜ ʙᴧɴɪʏᴧ <89186991+TeamBaniya@users.noreply.github.com>
2026-04-18 19:16:56 +0530

    Delete team/init.py

--- team/init.py
+++
@@ -1,5 +0,0 @@
-# Team mode module
-from .handlers import register_team_handlers
-from .game import create_team_game, join_team_a, join_team_b, start_team_match
-from .scoreboard import build_team_scoreboard
-from .score import get_team_live_score
