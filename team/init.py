be96a7bae170f9a18e0d3dd96b7dd37a143476a1
ꜱᴘᴧʀꜱʜ ʙᴧɴɪʏᴧ <89186991+TeamBaniya@users.noreply.github.com>
2026-04-18 19:00:54 +0530

    Create init.py

---
+++ team/init.py
@@ -0,0 +1,5 @@
+# Team mode module
+from .handlers import register_team_handlers
+from .game import create_team_game, join_team_a, join_team_b, start_team_match
+from .scoreboard import build_team_scoreboard
+from .score import get_team_live_score
