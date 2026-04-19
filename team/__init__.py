a6c01f3287424e3eefa426e623376792bae232b7
ꜱᴘᴧʀꜱʜ ʙᴧɴɪʏᴧ <89186991+TeamBaniya@users.noreply.github.com>
2026-04-19 15:13:44 +0530

    Delete team directory

--- team/__init__.py
+++
@@ -1,9 +0,0 @@
-# team/__init__.py
-from .create_team import register_create_team
-from .join_team import register_join_team
-from .match import register_match
-
-def register_team_handlers(app):
-    register_create_team(app)
-    register_join_team(app)
-    register_match(app)
